132cfd6eae46f7997f521e32fea289fcc439c188
Brett <154654520+silencer7777@users.noreply.github.com>
2025-04-11 13:34:58 -0700

    Create MealGenerator.js

---
+++ components/components/MealGenerator.js
@@ -0,0 +1,13 @@
+export default class MealGenerator {
+  constructor() {
+    this.cookbooks = {
+      "Run Fast. Eat Slow": { protein: 40, carbs: 30 },
+      "The Whole30": { protein: 35, carbs: 40 }
+    };
+    this.renderForm();
+  }
+
+  renderForm() {
+    // Generate meal plan UI
+  }
+}
